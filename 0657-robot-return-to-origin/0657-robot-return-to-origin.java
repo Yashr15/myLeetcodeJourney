432d2d9733082b5f4e3fc1432870fe9f52d5bf5e
YUVRAJ SINGH RATHORE <142774970+Yashr15@users.noreply.github.com>
2026-08-05 22:04:23 +0530

    Time: 5 ms (69.3%), Space: 46 MB (37.23%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.java
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.java
@@ -1,21 +1,18 @@
 class Solution {
     public boolean judgeCircle(String moves) {
-        int ucount = 0;
-        int dcount = 0;
-        int lcount = 0;
-        int rcount = 0;
-        for (int i=0; i<moves.length(); i++){
-            if(moves.charAt(i) == 'U'){
-                ucount++;
-            }else if(moves.charAt(i) == 'D'){
-                dcount++;
-            }else if(moves.charAt(i) == 'L'){
-                lcount++;
-            }else {
-                rcount++;
+        int x=0;
+        int y=0;
+        for(char c:moves.toCharArray()){
+            if(c=='U'){
+                y++;
+            }else if(c=='D'){
+                y--;
+            }else if(c=='R'){
+                x++;
+            }else{
+                x--;
             }
-        }
-        if( ucount == dcount && lcount == rcount) return true;
-        return false;
+       }
+       return x==0 && y==0;
     }
 }
