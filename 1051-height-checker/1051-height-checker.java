81480be76fa39909008f178606738d2b35e3eaf0
YUVRAJ SINGH RATHORE <142774970+Yashr15@users.noreply.github.com>
2026-08-16 21:34:32 +0530

    Time: 0 ms (100%), Space: 43.1 MB (92.43%) - LeetHub

--- 1051-height-checker/1051-height-checker.java
+++ 1051-height-checker/1051-height-checker.java
@@ -1,14 +1,25 @@
 class Solution {
     public int heightChecker(int[] heights) {
-        int n = heights.length;
-        int[] expected = heights.clone();
-        Arrays.sort(expected);
+        int[] count = new int[101];
+
+        for (int h : heights) {
+            count[h]++;
+        }
+
         int different = 0;
-        for(int i=0; i<n ; i++){
-            if(heights[i] != expected[i]){
-                different++;
+        int index = 0;
+
+        for (int h = 1; h <= 100; h++) {
+            while (count[h] > 0) {
+                if (heights[index] != h) {
+                    different++;
+                }
+
+                index++;
+                count[h]--;
             }
         }
+
         return different;
     }
 }
